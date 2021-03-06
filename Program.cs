66c3a0b79055aad8593da7072b2ec0c5f66dcdf2
ellintuba <tubatuba144@gmail.com>
2021-03-06 22:06:05 +0600

    switch case

--- Program.cs
+++ Program.cs
@@ -10,6 +10,29 @@ namespace MidTermLabAssignment2
     {
         static void Main(string[] args)
         {
+            string inputCommand;
+            while (true)
+            {
+                Console.WriteLine("Enter the command for :");
+                Console.WriteLine("Open a Bank account");
+                Console.WriteLine("Perform transactions for an account");
+                Console.WriteLine("Exit the application");
+                inputCommand = Console.ReadLine();
+                switch (inputCommand)
+                {
+                    case "open":
+                        Console.WriteLine(inputCommand);
+                        break;
+                    case "account":
+                        Console.WriteLine(inputCommand);
+                        break;
+                    case "quit":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid Command. Please Enter open or account or quit as command");
+                        break;
+                }
+            }
         }
     }
 }
